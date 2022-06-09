67b9036eec85fe0b80f8a5506596cfba170b833f
ZhiXiang <zhixiang8897@gmail.com>
2022-06-10 02:15:52 +0800

    dark

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/global.css';
+import { ThemeProvider } from 'next-themes'
 
-export default function App({ Component, pageProps }) {
+
+/*function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+*/
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,26 @@ import Date from '../components/date';
 
 import { getSortedPostsData } from '../lib/posts';
 
+import Darkmode from 'darkmode-js';
+
+const options = {
+  bottom: '64px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -47,3 +67,4 @@ export default function Home({ allPostsData }) {
     </Layout>
   );
 }
+
